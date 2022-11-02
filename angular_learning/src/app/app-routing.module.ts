6ee250a3ed45834728441bbe7279427b6e0e009a
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2022-11-02 14:34:23 +0700

    [MODIFIED] Routing

--- angular_learning/src/app/app-routing.module.ts
+++ angular_learning/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KasirComponent } from './transaksi/kasir/kasir.component';
+import { MenuComponent } from './transaksi/menu/menu.component';
+import { PaymentComponent } from './transaksi/payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/kasir'
+  },
+  {
+    path: 'kasir',
+    component: KasirComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: 'payment',
+    component: PaymentComponent
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KasirComponent } from './transaksi/kasir/kasir.component';
+import { MenuComponent } from './transaksi/menu/menu.component';
+import { PaymentComponent } from './transaksi/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KasirComponent,
+    MenuComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
